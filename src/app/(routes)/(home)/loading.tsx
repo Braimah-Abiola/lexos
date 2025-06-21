46683fbe0e26c1a498a6d102c6e2e384a7e6b0eb
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-06-21 19:21:26 +0000

    Fixes

---
+++ src/app/(routes)/(home)/loading.tsx
@@ -0,0 +1,13 @@
+import { Loader } from "lucide-react";
+
+const HomeLoading = () => {
+    return (
+        <div className=" w-full h-screen flex items-center justify-center">
+            <div className=" flex flex-col items-center gap-2">
+                <Loader className=" animate-spin text-primary" />
+            </div>
+        </div>
+    );
+}
+
+export default HomeLoading;
